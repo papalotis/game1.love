125e9faadd24f4ad2ea38cbaeb865816b87b0960
Panagiotis <papalotis1@gmail.com>
2018-02-06 01:00:19 +0100

    removed install of luaunit, test should fail

--- tests/player_tests.lua
+++ tests/player_tests.lua
@@ -8,7 +8,7 @@ local Player = require "src.Player"
 
 --tests to check if unit library works correctly
 function testPass()
-    lu.assertEquals(1, 1)
+    lu.assertEquals(1, 2)
     lu.assertEquals("string","string")
 end
 --
