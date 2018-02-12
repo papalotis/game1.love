4cc07b1ad5c40748a92d9d2a91153736ca799824
Panagiotis <papalotis1@gmail.com>
2018-02-12 18:03:25 +0100

    added "pseudo"-init methom

--- src/AlwaysActiveWall.lua
+++ src/AlwaysActiveWall.lua
@@ -1,12 +1,20 @@
 local Wall = require "src.Wall"
-local vector = require "src.vector"
 
 --Wall class, that is not affected by the user changing the active group
 --in the game
 local AlwaysActiveWall = Wall:extend()
 
+function AlwaysActiveWall.childinit (self)
+    self.isalwaysactivewallobject = true
+end
+
 function AlwaysActiveWall.update(self)
-    self.colour = game_colours.light_gray
+    self.copied = self.copied or false
+    if (not self.copied) then
+        self.copied = true
+        self.colour = game_colours.light_gray:clone()
+
+    end
     self.active = true
 end
 
